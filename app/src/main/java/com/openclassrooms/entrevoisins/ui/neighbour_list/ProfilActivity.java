d23db76a1ea70517df13c09eb0a4039bc556a91e
amine91480 <amine91480@hotmail.fr>
2020-11-27 13:26:39 +0100

    received the data of neighbour with intente.getStringExtra to the methode and connect the view

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfilActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfilActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,18 +27,23 @@ public class ProfilActivity extends AppCompatActivity {
 
     @BindView(R.id.show_name_neighbour)
     TextView mName;
+    @BindView(R.id.show_name_avatar_neighbour)
+    TextView mNameAvatar;
     @BindView(R.id.show_location_neighbour)
     TextView mLocation;
     @BindView(R.id.show_phone_neighbour)
     TextView mPhone;
-    @BindView(R.id.show_name_avatar_neighbour)
-    TextView mNameAvatar;
+    @BindView(R.id.show_adress_neighbour)
+    TextView mAdress;
     @BindView(R.id.show_about_neighbour)
     TextView mAbout;
     @BindView(R.id.show_avatar_neighbour)
     ImageView mAvatar;
+    @BindView(R.id.floatingActionButtonProfil)
+    ImageView mFav;
 
     private NeighbourApiService mApiService;
+
 
 
     @Override
@@ -45,26 +51,35 @@ public class ProfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
         ButterKnife.bind(this);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mApiService = DI.getNeighbourApiService();
         init();
     }
 
     private void init(){
-        Intent intent = getIntent();
-        String neighbourName =  intent.getStringExtra("neighbourName");
-        String neighbourLocation =  intent.getStringExtra("neighbourLocation");
-        String neighbourPhone =  intent.getStringExtra("neighbourPhone");
-        String neighbourAbout =  intent.getStringExtra("neighbourAbout");
-        String neighbourAvatar =  intent.getStringExtra("neighbourAvatar");
+        String neighbourName = "Name is not set";
+        String neighbourLocation = "Location is not set";
+        String neighbourPhone = "Phone number is not set";
+        String neighbourAbout = "About is not set";
+        String neighbourAvatar = "The URL AVATAR is not set";
 
-        Log.d("ProfilActivity+OnCreate","TOUT devrait s'afficher");
+        Intent intent = getIntent();
+        if( intent != null ) {
+            neighbourName =  intent.getStringExtra("neighbourName");
+            neighbourLocation =  intent.getStringExtra("neighbourLocation");
+            neighbourPhone =  intent.getStringExtra("neighbourPhone");
+            neighbourAbout =  intent.getStringExtra("neighbourAbout");
+            neighbourAvatar =  intent.getStringExtra("neighbourAvatar");
+        }
+
+        Log.d("ProfilActivity+OnCreate","Data transmis");
         Glide.with(this).load(neighbourAvatar)
+                .apply(RequestOptions.noTransformation())
                 .into(mAvatar);
         mName.setText(neighbourName);
         mNameAvatar.setText(neighbourName);
         mLocation.setText(neighbourLocation);
         mPhone.setText(neighbourPhone);
+        mAdress.setText("www.facebook/"+neighbourName.toLowerCase());
         mAbout.setText(neighbourAbout);
     }
 }
