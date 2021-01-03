d0bced20a5909f19920cd1bb1f95126d3fb61b5a
amine91480 <amine91480@hotmail.fr>
2021-01-03 11:59:12 +0100

    Change the directory folder

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
@@ -4,8 +4,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-import com.openclassrooms.entrevoisins.ui.neighbour_list.Favorite.NeighbourFavFragment;
-
 
 public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
 
