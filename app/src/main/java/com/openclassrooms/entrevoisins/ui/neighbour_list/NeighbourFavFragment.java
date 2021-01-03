883ccfeaf9fef3b9aa9d75b69cee217bc81d52af
amine91480 <amine91480@hotmail.fr>
2021-01-03 12:05:37 +0100

    Clean code

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourFavFragment.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourFavFragment.java
@@ -1,33 +1,23 @@
-package com.openclassrooms.entrevoisins.ui.neighbour_list.Favorite;
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.model.Neighbour;
 import com.openclassrooms.entrevoisins.service.NeighbourApiService;
-import com.openclassrooms.entrevoisins.ui.neighbour_list.MyNeighbourRecyclerViewAdapter;
 
-import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
 import java.util.List;
-
-import butterknife.BindView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -37,13 +27,8 @@ import butterknife.BindView;
 public class NeighbourFavFragment extends Fragment {
 
     private List<Neighbour> mFavorites;
-
     private NeighbourApiService mApiService;
     private RecyclerView mRecyclerView;
-    private MyNeighbourRecyclerViewAdapter mAdapter;
-
-    @BindView(R.id.item_list_delete_button)
-    ImageView deleteButton;
 
     /**
      * Create and return a new instance
@@ -63,7 +48,7 @@ public class NeighbourFavFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_neighbour_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_neighbour_fav, container, false);
         Context context = view.getContext();
         mRecyclerView = (RecyclerView) view;
         mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
@@ -74,8 +59,7 @@ public class NeighbourFavFragment extends Fragment {
     @Subscribe
     private void initRecyclerView() {
         mFavorites = mApiService.getFavorites();
-        mAdapter = new MyNeighbourRecyclerViewAdapter(mFavorites);
-        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(new MyNeighbourRecyclerViewAdapter(mFavorites));
     }
 
     @Override
